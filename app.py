36dc0971b7745919ea4bdfefe85192df6d98226e
alan <alanl07905@gmail.com>
2024-10-09 10:46:39 -0400

    add commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, jsonify
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+app = Flask(__name__)
+
+
+# TODO: Fetch dataset, initialize vectorizer and LSA here
+newgroups = fetch_20newsgroups(subset='all')
+vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'))
+X = vectorizer.fit_transform(newgroups.data)
+svd = TruncatedSVD(n_components=100)
+X_lsa = svd.fit_transform(X)
+
+
+def search_engine(query):
+    """
+    Function to search for top 5 similar documents given a query
+    Input: query (str)
+    Output: documents (list), similarities (list), indices (list)
+    """
+    # TODO: Implement search engine here
+    # return documents, similarities, indices 
+    query_vec = vectorizer.transform([query])
+    query_lsa = svd.transform(query_vec)
+    similarities = cosine_similarity(query_lsa, X_lsa).flatten()
+    top_indices = np.argsort(similarities)[-5:][::-1]
+
+    documents = [newgroups.data[i] for i in top_indices]
+    similarities = [similarities[i] for i in top_indices]
+    indices = top_indices.tolist()
+
+    return documents, similarities, indices
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+    documents, similarities, indices = search_engine(query)
+    return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices}) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
